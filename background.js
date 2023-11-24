029f3b524c28ea8ce18577dc83a4eb9562a45401
edcous <49954272+edcous@users.noreply.github.com>
2023-11-23 23:14:05 -0600

    initial work on firebase registration

--- background.js
+++ background.js
@@ -1,9 +1,23 @@
-const url = "https://loaners.codeths.dev"
+const url = "http://localhost:3000"
+const sender_id = "543663475808" // beta ID
+const serial = "test"
 
 chrome.runtime.onInstalled.addListener(async ({ reason }) => {
     if (reason !== 'install') {
       return;
     }
-    const response = await fetch(`${url}/api/v1/ext/register`, {method: "POST", body: JSON.stringify({"serial": serial, "alertToken": alertToken})})
+    registerFirebase()
 });
-  
+
+function registerFirebase(){
+  chrome.gcm.register([sender_id], firebaseCallback)
+}
+
+let firebaseCallback = async function(alertToken){
+  const response = await fetch(`${url}/api/v1/ext/register`, {method: "POST", body: JSON.stringify({"serial": serial, "alertToken": alertToken})})
+  if(response.status === 200){
+    chrome.storage.local.set({"deviceStatus": response.data.deviceStatus, "loanerStatus": response.data.loanerStatus, "startDate": response.data.startDate}).then(() => {
+      console.log("Value is set");
+    });  
+  }
+}
